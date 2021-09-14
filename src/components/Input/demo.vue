dbd0140f95a4a922dfc0c131a20b985ee76337ab
shoushangdetuolaji <tklijunhao@qq.com>
2021-09-15 00:00:19 +0900

    input组件

---
+++ src/components/Input/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo-box">
+    <p>text: {{ text }}</p>
+    <a-input
+      v-model="text"
+      readonly="true"
+      placeholder="请输入"/>
+  </div>
+</template>
+
+<script lang="tsx">
+import {defineComponent, ref} from 'vue';
+import AInput from './index'
+
+export default defineComponent({
+  name: "InputDemo",
+  components: { AInput },
+  setup(props, { emit }) {
+    const text = ref('')
+    return {
+      text
+    }
+  }
+})
+</script>
+
+<style lange="scss">
+
+</style>

195b6e48fed2b3fff5358db202398b3c9536b5b4
shoushangdetuolaji <tklijunhao@qq.com>
2021-09-15 12:58:23 +0900

    add part1

---
+++ src/components/Form/FormItem.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue;
+export default defineComponent({
+  name: 'AFormItem',
+  setup(props, { emit }) {
+    return () => {
+      return (
+        <div>
+          AFormItem
+        </div>
+      )
+    }
+  }  
+})
